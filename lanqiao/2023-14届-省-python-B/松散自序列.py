b0d87d2abd5ffb826a5b9ae59ff979c4c2e3b388
LR2933 <r2933985616@gmail.com>
2026-04-09 11:01:35 +0800

    update

---
+++ lanqiao/2023-14届-省-python-B/松散自序列.py
@@ -0,0 +1,4 @@
+import sys
+input_data = sys.stdin.read().strip()
+s = [i for i in input_data]
+
--- temp.py
+++ temp.py
@@ -1,5 +1,26 @@
-from functools import lru_cache
+import sys
+from collections import defaultdict
 def solve():
+    _ = int(sys.stdin.readline())
+    data = defaultdict(list)
+    lines = sys.stdin.readlines()
+    for line in lines:
+        a, b = map(int, line.split())
+        data[b].append(a)
+
+    count = 0
+    tili_sum = 0
+    for i in sorted(data): #最多允许消耗的体力
+        data[i].sort()
+        for j in data[i]: #需要消耗的体力
+            if tili_sum <= i:
+                tili_sum += j
+                count += 1
+            else:
+                break
+         
+    print(count)
 
 if __name__ == "__main__":
     solve()
+
